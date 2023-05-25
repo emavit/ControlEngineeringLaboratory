7d1b96dda0b171f7023b8f977580e76ec4235fcc
perondavid <perondavid@pcfa07.dei.unipd.it>
2023-05-25 15:59:43 +0200

    finished last lab

--- LAB4/Laboratory/LAB4datalog.m
+++ LAB4/Laboratory/LAB4datalog.m
@@ -2,16 +2,18 @@
 %window
 
 % packet specifier (must match the specifier in the "Packet Send" block)
-packetSpec = {'2*uint8', '2*int32', '2*single', 'single'};
+%packetSpec = {'2*uint8', '2*int32', '2*single', 'single'};
+packetSpec = {'single','single','single','single','2*single','2*single'};
+
 
 % interactive data−logging
-data = serial_datalog( 'COM3', ... % serial port name
+data = serial_datalog( 'COM10', ... % serial port name
 packetSpec, ... % packet specifier
 'BaudRate', 38400, ... % serial port baud rate
 'TxSampleTime', 0.01, ... % TX sample time
 'TxStartCmd', 65, ... % TX start cmd (default)
 'TxStopCmd', 66, ... % TX stop cmd (default)
-'BufferSize', 10000, ... % RX buffer size
+'BufferSize', 120000, ... % RX buffer size
 'PlotWidth', 5, ... % time−axis width (default)
 'PlotRefreshRatio', 10, ... % plot refresh ratio
-'PlotList', [1,2,3,4]); % plot list
+'PlotList', [1,2,3,4,5,6]); % plot list
